eebeefe56bc7549c79bfce91b96640e7ee618665
GabrielZocFavarin <gabrielfava.pvd@gmail.com>
2025-11-21 21:10:23 -0300

    Finalizado projeto, aprendizado concluido.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from services import salvar_valor
 if __name__ == "__main__":
     valor = input("Digite o nome do valor a ser salvo: ")
     registro = salvar_valor(valor)
-    print(f"Valor salvo com ID: {valor.id} e Nome: {valor.nome}")
+    print(f"Valor salvo {valor} no banco de dados")
